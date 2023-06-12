2e8d7783a8f17d0e5cd7709e6680b1413fe22438
Yuvadi29 <adityatrivedi612@gmail.com>
2023-06-12 16:41:04 +0530

    Class Based Component in React

--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -1,11 +1,23 @@
-import React from 'react'
+// import React from 'react'
 
-const Button = () => {
-  return (
-    <div>
+// const Button = () => {
+//   return (
+//     <div>
+//         <h1>Button</h1>
+//     </div>
+//   )
+// }
+
+// export default Button;
+
+import React, { Component } from 'react'
+
+export default class Button extends Component {
+  render() {
+    return (
+      <div>
         <h1>Button</h1>
-    </div>
-  )
+      </div>
+    )
+  }
 }
-
-export default Button;
