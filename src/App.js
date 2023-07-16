495ffe65e263463e52d5bc314ba8ded06c8fa91a
Yuvadi29 <adityatrivedi612@gmail.com>
2023-07-16 20:27:49 +0530

    UseState Hook Completed ✅

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
-import React from 'react'
-import Product from './Product'
-import ProductDetails from './ProductDetails'
+import React from 'react';
+import { useState } from 'react';
 
 const App = () => {
+
+  const [text, setText] = useState("");
+  const [number, setNumber] = useState(0);
+  const [bool, setBool] = useState(true);
+  const [arr, setArr] = useState([]);
+
+  const handleSubmit = () => {
+    console.log(text);
+    console.log(number);
+    bool ? setBool(false) : setBool(true);
+    console.log(bool);
+    arr.push(text);
+    console.log(arr);
+  }
+
   return (
-    <div>
-      <Product name="Iphone XR" />
-      <ProductDetails price="Rs.200" rating="4 Star"/>
-      <Product name="Iphone 14" />
-      <ProductDetails price="Rs.2000" rating="5 Star"/>
-      <Product name="Iphone SE" />
-      <ProductDetails price="Rs.2200" rating="4 Star"/>
+    <div>App
+      <input type="text" value={text} onChange={(e) => setText(e.target.value)} />
+      <input type="number" value={number} onChange={(e) => setNumber(e.target.value)} />
+      <button type="submit" onClick={() => handleSubmit()}>Boolean</button>
+      <button type="submit" onClick={() => handleSubmit()}>Submit</button>
     </div>
   )
 }
 
-export default App
+export default App;
