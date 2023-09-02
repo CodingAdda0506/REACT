e9f92700a4cba8d274d539347ba6e3589927c89a
Yuvadi29 <adityatrivedi612@gmail.com>
2023-09-02 21:04:00 +0530

    UseMemo Hook Completed

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useMemo, useState } from 'react'
 import Card from "./components/Card/Card";
 import { SmoothScroll } from './Lenis';
 import "./App.css";
@@ -7,19 +7,41 @@ import UseContext from "./context/useContext";
 const App = () => {
   SmoothScroll();
 
-  const ytData = {
-    Name: "Coding Adda",
-    Channel_Age: 2.5,
-    Member1: "Aditya",
-    Member2: "Hari"
+  const [number, setNumber] = useState(0);
+  const [show, setShow] = useState(true);
+
+  const incrementNumber = () => {
+    return setNumber(number + 1);
   }
 
+  const getNumber = (num) => {
+    console.log("The Number is : ", num);
+    for (let i = 0; i < 1000000000; i++) { }
+    return num;
+  }
+
+  // const data = getNumber(number);
+
+  const data = useMemo(() => {
+    return getNumber(number);
+  }, [number]);
+
   return (
-    <UseContext.Provider value={ytData}>
-      <div className="main-container">
-        <Card />
-      </div>
-    </UseContext.Provider>
+    <div>
+      <button
+        onClick={incrementNumber}
+      >
+        increment
+      </button>
+      <p>
+        Number is {data}.
+      </p>
+      <button
+        onClick={() => setShow(!show)}
+      >
+        {show ? "click me" : "click me again"}
+      </button>
+    </div>
   )
 }
 
