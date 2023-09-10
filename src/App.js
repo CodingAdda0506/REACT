627d28ca369d40cf78ffe37ca394a03075899ffc
Yuvadi29 <adityatrivedi612@gmail.com>
2023-09-10 16:07:52 +0530

    custom Hook completed --> useMousePosition Hook

--- src/App.js
+++ src/App.js
@@ -1,48 +1,15 @@
-import React, { useMemo, useState } from 'react'
-import Card from "./components/Card/Card";
-import { SmoothScroll } from './Lenis';
-import "./App.css";
-import UseContext from "./context/useContext";
+import React from 'react';
+import useMousePosition from './utils/useMousePosition';
 
 const App = () => {
-  SmoothScroll();
+  const {x, y} = useMousePosition();
 
-  const [number, setNumber] = useState(0);
-  const [show, setShow] = useState(true);
-
-  const incrementNumber = () => {
-    return setNumber(number + 1);
-  }
-
-  const getNumber = (num) => {
-    console.log("The Number is : ", num);
-    for (let i = 0; i < 1000000000; i++) { }
-    return num;
-  }
-
-  // const data = getNumber(number);
-
-  const data = useMemo(() => {
-    return getNumber(number);
-  }, [number]);
-
+  console.log(`${x} < ==== > ${y}`);
   return (
     <div>
-      <button
-        onClick={incrementNumber}
-      >
-        increment
-      </button>
-      <p>
-        Number is {data}.
-      </p>
-      <button
-        onClick={() => setShow(!show)}
-      >
-        {show ? "click me" : "click me again"}
-      </button>
+      Custom React Hook
     </div>
   )
 }
 
-export default App
+export default App;
---
+++ src/utils/useMousePosition.js
@@ -0,0 +1,23 @@
+// {x: val , y: val }
+
+import { useEffect, useState } from "react";
+
+export default function useMousePosition() {
+
+    const [ mousePosition, setMousePosition] = useState({x: 0 , y: 0});
+
+    const updateMousePosition = (e) => {
+        setMousePosition({x: e.clientX, y: e.clientY});
+    }
+
+    useEffect(() => {
+        window.addEventListener("mousemove", updateMousePosition);
+
+        // cleanup function
+        return () => {
+            window.removeEventListener("mousemove", updateMousePosition);
+        }
+    }, []);
+
+    return mousePosition;
+}
