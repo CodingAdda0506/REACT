58aae2290cbe4196049573b14cbd9be6db542b92
Yuvadi29 <adityatrivedi612@gmail.com>
2023-10-01 17:07:23 +0530

    Routes in React

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
 import React from 'react';
-import useMousePosition from './utils/useMousePosition';
 
 const App = () => {
-  const {x, y} = useMousePosition();
-
-  console.log(`${x} < ==== > ${y}`);
   return (
-    <div>
-      Custom React Hook
-    </div>
+    <div>App</div>
   )
 }
 
-export default App;
+export default App
--- src/index.js
+++ src/index.js
@@ -2,16 +2,33 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Product from './components/Product';
+import User from './components/User';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Home</div>,
+  },
+  {
+    path: "/about",
+    element: <div>About</div>,
+  },
+  {
+    path: "/experience",
+    element: <div>Experience</div>,
+  },
+  {
+    path: "/contact",
+    element: <div>Contact</div>,
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
+    {/* <App /> */}
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
